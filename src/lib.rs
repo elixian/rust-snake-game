7c22ec113ee99575175dc0a28cb8a8f399fa37a0
Franck COLOMBO <franck.colombo@cnav.fr>
2022-04-05 18:34:36 +0200

    Fisrt commit

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+use wasm_bindgen::prelude::*;
+use wee_alloc::WeeAlloc;
+
+#[global_allocator]
+static ALLOC: WeeAlloc = WeeAlloc::INIT;
+
+#[wasm_bindgen]
+pub struct World {
+    width : usize
+}
+#[wasm_bindgen]
+impl World {
+    pub fn new()-> World {
+        World { width: 10}
+    }
+
+    pub fn width( &self )-> usize{
+        self.width
+    }
+}
+
